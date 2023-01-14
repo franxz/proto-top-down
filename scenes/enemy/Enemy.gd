91bad3ac2dbcb0a31f0b8c8e7aa3c18935d677e0
Franco Martucci <franco.martucci.21@gmail.com>
2023-01-14 13:34:50 -0300

    Add take_damage animation to the Enemy

--- scenes/enemy/Enemy.gd
+++ scenes/enemy/Enemy.gd
@@ -1,10 +1,11 @@
 extends RigidBody2D
 
-onready var sprite_node : Sprite = get_node("Sprite")
-
 var max_health := 3
 var current_health := max_health
 var steer_agent = SteeringBehaviorsAgent.new()
+
+onready var sprite_node : Sprite = get_node("Sprite")
+onready var animation_node : AnimationPlayer = get_node("AnimationPlayer")
 
 func init(position_: Vector2) -> void:
   position = position_
@@ -24,6 +25,7 @@ func _integrate_forces(state):
   
 
 func take_damage(impulse: Vector2) -> void:
+  animation_node.play("take_damage")
   apply_central_impulse(impulse)
   current_health -= 1
   if current_health <= 0:
