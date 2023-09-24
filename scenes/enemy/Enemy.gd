90ca8c5fb9fcb8b362ff86d9faa20a55559eed87
franxz <franco.martucci.21@gmail.com>
2023-09-23 21:26:39 -0300

    Add a Steering Behaviors system

--- scenes/enemy/Enemy.gd
+++ scenes/enemy/Enemy.gd
@@ -8,29 +8,39 @@ onready var sprite_node : Sprite = get_node("Sprite")
 onready var animation_node : AnimationPlayer = get_node("AnimationPlayer")
 
 func init(position_: Vector2) -> void:
-  position = position_
+	position = position_
 
 
 func _ready():
-  add_to_group('enemies')
-  steer_agent.init(self, 800, 500, Global.player)
+	add_to_group('enemies')
+	steer_agent.init(self, 800, 650)
+	steer_agent.seek(Global.player, 1.2)
+	steer_agent.obstacle_avoidance(30, 4, 1.3)
 
 
 func _physics_process(delta):
-  steer_agent.physics_process(delta)
+	steer_agent.physics_process(delta)
 
 
 func _integrate_forces(state):
-  steer_agent.integrate_forces(state)
-  
+	steer_agent.integrate_forces(state)
+	update()
+
 
 func take_damage(impulse: Vector2) -> void:
-  animation_node.play("take_damage")
-  apply_central_impulse(impulse)
-  current_health -= 1
-  if current_health <= 0:
-	  queue_free()
+	animation_node.play("take_damage")
+	apply_central_impulse(impulse)
+	current_health -= 1
+	if current_health <= 0:
+		queue_free()
 
 
-func debug_highlight() -> void:
-  sprite_node.modulate = Color.yellow
+func debug_highlight(active := true) -> void:
+	if active:
+		sprite_node.modulate = Color.yellow
+	else:
+		sprite_node.modulate = Color.red
+
+
+func _draw():
+	draw_line(Vector2.ZERO, linear_velocity.rotated(-rotation), Color.gold)
--- scripts/SteeringBehaviorsAgent.gd
+++ scripts/SteeringBehaviorsAgent.gd
@@ -1,27 +1,116 @@
 extends Reference
 class_name SteeringBehaviorsAgent
 
-# this could be improved to:
-# - select different behaviors
-# - combine them with weights
+# TODO: use delta in the steering force calc?
+# TODO: add remove_behavior
+
+enum BEHAVIORS { SEEK, OBSTACLE_AVOIDANCE }
+
+var behaviors = []
+var behaviors_data = []
+var behaviors_weights = []
 
 var agent_node : RigidBody2D = null
-var seek_target : Node2D = null
-var max_force: float
-var max_speed: float
+var max_force : float
+var max_speed : float
 
-func init(agent_node_ : RigidBody2D, max_force_: float, max_speed_: float, seek_target_: Node2D) -> void:
+func init(agent_node_ : RigidBody2D, max_force_: float, max_speed_: float) -> void:
 	agent_node = agent_node_
 	max_force = max_force_
 	max_speed = max_speed_
-	seek_target = seek_target_
-
 
 func physics_process(_delta: float) -> void:
-	var desired_velocity = (seek_target.global_position - agent_node.global_position).normalized() * max_speed
-	var steering_force = (desired_velocity - agent_node.linear_velocity).limit_length(max_force)
-	agent_node.applied_force = steering_force
+	var steering_force := Vector2.ZERO
+	for i in range(behaviors.size()):
+		steering_force += _call_behavior(behaviors[i], behaviors_data[i]) * behaviors_weights[i]
+	agent_node.applied_force = steering_force.limit_length(max_force)
 
 
 func integrate_forces(state: Physics2DDirectBodyState) -> void:
 	state.linear_velocity = state.linear_velocity.limit_length(max_speed)
+
+
+func _add_behavior(behavior: int, data: Object, weight: float) -> int:
+	behaviors.append(behavior)
+	behaviors_data.append(data)
+	behaviors_weights.append(weight)
+	return behaviors.size() - 1
+
+
+func _call_behavior(behavior: int, data) -> Vector2:
+	match behavior:
+		BEHAVIORS.SEEK:
+			return _seek(data as SeekData)
+		BEHAVIORS.OBSTACLE_AVOIDANCE:
+			return _obstacle_avoidance(data as ObstacleAvoidanceData)
+	return Vector2.ZERO
+
+
+##############################
+# STEERING BEHAVIORS (DATA)
+##############################
+class SeekData:
+	var seek_target : Node2D
+
+class ObstacleAvoidanceData:
+	var agent_area : Area2D
+	var obstacles_layer : int
+
+
+##############################
+# STEERING BEHAVIORS (INIT)
+##############################
+func seek(target: Node2D, weight := 1.0) -> int:
+	var data = SeekData.new()
+	data.seek_target = target
+	return _add_behavior(BEHAVIORS.SEEK, data, weight)
+
+
+ # obstacles_layer -> collision layer of the obstacles
+func obstacle_avoidance(agent_radius: float, obstacles_layer: int, weight := 1.0) -> int:
+	var shape = RectangleShape2D.new() # TODO: use circle shape
+	shape.extents = Vector2(agent_radius, agent_radius)
+	
+	var collision_shape = CollisionShape2D.new()
+	collision_shape.position = Vector2.ZERO # TODO: check if this is necessary
+	collision_shape.shape = shape
+
+	var agent_area = Area2D.new()
+	agent_area.position = Vector2.ZERO # TODO: check if this is necessary
+	agent_area.collision_layer = 0 # TODO: check if this is necessary
+	agent_area.collision_mask = 4
+	
+	agent_node.add_child(agent_area)
+	agent_area.add_child(collision_shape)
+	
+	var data = ObstacleAvoidanceData.new()
+	data.agent_area = agent_area
+	data.obstacles_layer = obstacles_layer
+	return _add_behavior(BEHAVIORS.OBSTACLE_AVOIDANCE, data, weight)
+
+
+##############################
+# STEERING BEHAVIORS (PROCESS)
+##############################
+func _seek(data: SeekData) -> Vector2:
+	var desired_velocity = (data.seek_target.global_position - agent_node.global_position).normalized() * max_speed
+	return desired_velocity - agent_node.linear_velocity
+
+
+func _obstacle_avoidance(data: ObstacleAvoidanceData) -> Vector2:
+	var collider_position = null
+
+	if data.agent_area.get_overlapping_areas().size() > 0:
+		collider_position = data.agent_area.get_overlapping_areas()[0].global_position
+	else:
+		var space_state = agent_node.get_world_2d().direct_space_state
+		var raycast_result = space_state.intersect_ray(agent_node.global_position, agent_node.global_position + agent_node.linear_velocity, [], data.obstacles_layer, false, true) # TODO: add option to check bodies?
+		if raycast_result:
+			collider_position = raycast_result.collider.global_position
+
+	if !collider_position:
+		return Vector2.ZERO
+	
+	var right_vector = agent_node.linear_velocity.rotated(PI / 2)
+	var steering_force = (collider_position - agent_node.global_position).project(right_vector)
+	return steering_force.normalized() * max_force * (-1) # TODO: improve this calc?
